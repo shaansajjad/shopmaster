1223d0f035ee1948d73c6dd6ccb21056e2d79282
Sajjad khan <shaankhan178@gmail.com>
2020-02-27 16:05:14 +0530

    added pdf-img-convertor

--- src/components/pdf-to-img/PdfToImg.js
+++ src/components/pdf-to-img/PdfToImg.js
@@ -30,7 +30,7 @@ class PdfToImg extends React.Component {
     // console.log("Updtaed state is", this.state);
 
     axios({
-      url: "http://elearningapi.appskeeper.com/pdf",
+      url: "https://elearningapi.appskeeper.com/pdf",
       method: "POST",
       headers: {
         "Content-Type": "multipart/form-data"
